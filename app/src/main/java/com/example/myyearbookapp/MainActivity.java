88096f82dcf873e4d4177e26b54fd8e4ad01cd20
Kate <kate.wagner@outlook.com>
2019-02-26 20:22:24 -0500

    webview

--- app/src/main/java/com/example/myyearbookapp/MainActivity.java
+++ app/src/main/java/com/example/myyearbookapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.myyearbookapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webview;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //ImageView iv = (ImageView)findViewById(R.id.imageView5);
+        //iv.setOnClickListener(new View.OnClickListener() {
+
+
+//        }
+
+    }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, webview.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/myyearbookapp/webview.java
+++ app/src/main/java/com/example/myyearbookapp/webview.java
@@ -1,22 +1,43 @@
+
+
+
+
+
+
+
+
+
+
+
+
 package com.example.myyearbookapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class webview extends AppCompatActivity {
 
+    private WebView webview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_webview);
+
+
+        webview = (WebView) findViewById(R.id.katespage);
+
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl("https://www.google.com");
     }
 
-    public void loadUrl (String url)
-    {
-        webview myWebView = (webview) findViewById(R.id.webview);
-        myWebView.loadUrl("http://www.google.com");
 
-    }
+
+
 
 
 
